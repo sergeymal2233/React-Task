627f22b31d62c91b2bc805ff35947e52d1f29bab
sergaymal2233 <sergeymal2233@gmail.com>
2024-02-05 19:20:57 +0200

    Not found page realisation

---
+++ src/constants/notFound.ts
@@ -0,0 +1,7 @@
+export const notFoundReasons = [
+  "Error in the URL: Make sure you've entered the correct URL",
+
+  'Outdated link: This page may have been moved or the link to it may be outdated',
+
+  'Temporary unavailability: The server hosting our app may be temporarily unavailable.',
+]
--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,34 @@
+import { useNavigate } from 'react-router-dom'
+import styles from './NotFound.module.scss'
+import Typography from '@/components/UI/Typography'
+import Button from '@/components/UI/Button'
+import { notFoundReasons } from '@/constants/notFound'
+
 const NotFound = () => {
+  const navigate = useNavigate()
+
+  const handleNavigateToHome = () => {
+    navigate('/')
+  }
+
   return (
-    <div>NotFound</div>
+    <div className={styles.body}>
+      <Typography variant='h3'>
+        We're sorry for the inconvenience, but it looks like the page you're
+        looking for couldn't be found.
+      </Typography>
+
+      <Typography variant='h4'>Potential causes:</Typography>
+
+      <ul className={styles.list}>
+        {notFoundReasons.map((reason) => (
+          <li className={styles.item} key={reason}>
+            {reason}
+          </li>
+        ))}
+      </ul>
+      <Button onClick={handleNavigateToHome}>Go Home</Button>
+    </div>
   )
 }
 
